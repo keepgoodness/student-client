a45ad97a9d5d6d0a0b1fbaa3ac6c3232f2f50654
keepgoodness <yordanlazarov83@gmail.com>
2018-12-19 17:06:39 +0200

    first commit

---
+++ studentapi_client/src/main/java/com/yordan/student_client/controllers/StudentController.java
@@ -0,0 +1,35 @@
+package com.yordan.student_client.controllers;
+
+import com.yordan.student_client.services.RestClientStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ *
+ * @author Jordan
+ */
+@Controller
+public class StudentController {
+
+	private final RestClientStudentService studentService;
+
+	@Autowired
+	public StudentController(RestClientStudentService studentService) {
+		this.studentService = studentService;
+	}
+
+	@GetMapping("students")
+	public String getAllStudents(Model model) {
+		model.addAttribute("students", studentService.findAllStudents());
+		return "students";
+	}
+
+	@GetMapping("student/{id}")
+	public String getstudentById(@PathVariable String id, Model model) {
+		model.addAttribute("student", studentService.findStudentById(id));
+		return "singlestudent";
+	}
+}
---
+++ studentapi_client/src/main/java/com/yordan/student_client/services/RestClientStudentService.java
@@ -0,0 +1,36 @@
+package com.yordan.student_client.services;
+
+import com.yordan.student_client.models.Student;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Jordan
+ */
+@Service
+public class RestClientStudentService {
+
+	private final RestTemplate restTemplate;
+	private final String GET_ALL_STUDENTS_URL = "http://localhost:8095/api/students/all";
+	private final String GET_STUDENT_URL_BY_ID = "http://localhost:8095/api/students/";
+
+	@Autowired
+	public RestClientStudentService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public List<Student> findAllStudents() {
+		return Arrays.stream(restTemplate.getForObject(GET_ALL_STUDENTS_URL, Student[].class))
+				.collect(Collectors.toList());
+	}
+
+	public Student findStudentById(String id) {
+		return restTemplate.getForObject(GET_STUDENT_URL_BY_ID.concat(id), Student.class);
+	}
+
+}
